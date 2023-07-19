8ccbcde2d98b0655e8d7b47a01b2638764a7d028
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-19 07:01:03 +0300

    Proje dosyası ekle.

---
+++ StudyingMethod7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace StudyingMethod7
+{
+    internal class Program
+    {
+        static void Hobby(string hobby = "Coding")
+        {
+            Console.WriteLine(hobby);
+        }
+        static void Main(string[] args)
+        {
+            Hobby("Gardening");
+            Hobby("Reading book");
+            Hobby();
+            Hobby("Swimming");
+            Hobby("Fishing");
+
+            Console.Read();
+        }
+    }
+}
